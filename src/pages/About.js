5afd900dad262a1b48593ad38727bf27259791b1
sanarya1 <sanaryahamakarim1@gmail.om>
2022-09-04 15:01:26 +0200

    changing some details in the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -3,7 +3,7 @@ import Navigation from "../components/Navigation";
 export default function About() {
   return (
     <div className="w-full h-[100vh]">
-      <div className="w-full h-[12vh] flex items-center justify-center fixed top-[-1]">
+      <div className="w-full h-[12vh] flex items-center justify-center">
         <Navigation />
       </div>
       <div className="w-full h-[35%]  flex items-center justify-center gap-[2%] mt-[3%]">
@@ -23,7 +23,7 @@ export default function About() {
           </div>
         </div>
       </div>
-      <div className="w-full h-[50%] mt-[3%] pr-[5%] flex flex-col justify-center">
+      <div className="w-[70%] h-[50%] mt-[3%] pr-[5%] flex flex-col justify-center">
         <h5 className="rudaw-font text-black text-[27px]">ڕۆزبێ تراڤێڵ</h5>
         <p className="rudaw-font mt-[2%]">کوردستان پڕە لە سەرسوڕمان. سەفەر پەیوەندی بە ئەگەر و گەڕان و دۆزینەوە و زانینەوە هەیە. ئێمە پسپۆڕین لە پلاندانان بۆ جەژنە ناوازەکان لە شوێنەکانی سەرتاسەری کوردستان</p>
         <p className="rudaw-font mt-[2%]">شارەزایی ئێمە دروستکردنی گەشتی سەرنجڕاکێشە کە چەندین چالاکی تێدابێت. گەشتەکانمان پێکدێت لە سەردانی شوێنە گرینگەکان، ناسینی کولتوری ناوچەکە، گەڕان بەناو سروشت، خواردن لە دەرەوە، کاتە پشووەکان، و چەندین سوپرایزی تر. ئێمە تێدەگەین کە کڕیارەکانمان ئەزموونێکی تایبەتیان دەوێت و ئێمە ئامادەین بۆ ڕێکخستن و پلاندانان!</p>
